46bd5d597268db53504a6422bdddb923f38efcf2
anuragsetia <anurag.setia@gmail.com>
2021-04-17 18:40:10 +0530

    exclude confirmed from growth

--- covid19india.py
+++ covid19india.py
@@ -21,16 +21,17 @@ def new_and_recovered(df):
 
 def cases_growth_chart(data):
     data['Total Active'] = data['Total Confirmed'] - data['Total Recovered'] - data['Total Deceased']
-    data['GR (C)'] = data['Total Confirmed'].pct_change()
-    data['GR (A)'] = data['Total Active'].pct_change()
+    # data['GR (C)'] = data['Total Confirmed'].pct_change()
+    # data['5-day GR (C)'] = data['GR (C)'].rolling(5).mean()
+    # data['5-day GR (C)'] = data['5-day GR (C)'].round(3)*100
 
-    data['5-day GR (C)'] = data['GR (C)'].rolling(5).mean()
-    data['5-day GR (C)'] = data['5-day GR (C)'].round(3)*100
+    data['GR (A)'] = data['Total Active'].pct_change()
     data['5-day GR (A)'] = data['GR (A)'].rolling(5).mean()
-    data['5-day GR (A)'] = data['5-day GR (A)'].round(3)*100
+    data['Growth Rate (5d Moving Avg)'] = data['5-day GR (A)'].round(3)*100
     #lines = data.plot.line()
 #    print(data)
-    return data.loc[:,['Date', '5-day GR (C)','5-day GR (A)']]
+    # return data.loc[:,['Date', '5-day GR (C)','5-day GR (A)']]
+    return data.loc[:,['Date', 'Growth Rate (5d Moving Avg)']]
 
 def cases_total_chart(data):
     data['Total Active'] = data['Total Confirmed'] - data['Total Recovered'] - data['Total Deceased']
